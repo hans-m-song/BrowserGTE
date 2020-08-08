6700c2d3bb6622e0998f23f456f1463de24fe33b
Hans Song <hans.m.song@gmail.com>
2020-08-08 22:53:40 +1000

    styling options page, basic form functionality

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,20 +1,49 @@
 import {Header, Sender} from '@util/constants';
 import {message} from '@util/message';
+import {El, element, resizeTextArea, Section, setMessage} from './utils';
 
 const {send} = message(Sender.Options);
 
-const getElement = (id: string): HTMLElement => document.getElementById(id)!;
+Array.from(document.getElementsByTagName('textarea')).forEach((textArea) => {
+  resizeTextArea(textArea);
+  textArea.addEventListener('input', () => resizeTextArea(textArea));
+});
 
-const importBtn = getElement('import');
-const exportBtn = getElement('export');
-const messageDiv = getElement('message');
+[Section.Import, Section.Export].forEach(
+  (section) =>
+    (element(section, El.Clear).onclick = () => {
+      element(section, El.Input).value = '';
+      setMessage(section, '');
+      resizeTextArea(element(section, El.Input));
+    }),
+);
 
-importBtn.onclick = () => {
-  messageDiv.innerText = 'export';
-  send(Header.IMPORT, {});
+element(Section.Import, El.Submit).onclick = async () => {
+  try {
+    const input = element(Section.Import, El.Input);
+    const data = JSON.parse(input.value);
+    const response = await send(Header.IMPORT, data);
+    console.log('import response', response);
+    setMessage(Section.Import, `${response.header} ${response.sender}`);
+  } catch (e) {
+    console.error(e);
+    setMessage(Section.Import, `Error: ${e.message}`);
+  }
 };
 
-exportBtn.onclick = () => {
-  messageDiv.innerText = 'export';
-  send(Header.EXPORT, {});
+element(Section.Export, El.Submit).onclick = async () => {
+  try {
+    const response = await send(Header.EXPORT);
+    console.log('export response', response);
+    const data = JSON.stringify(response.data, null, 4);
+
+    const inputEl = element(Section.Export, El.Input);
+    inputEl.value = data;
+    resizeTextArea(inputEl);
+
+    setMessage(Section.Export, `${response.header} ${response.sender}`);
+  } catch (e) {
+    console.error(e);
+    setMessage(Section.Export, `Error: ${e.message}`);
+  }
 };
---
+++ src/options/utils.ts
@@ -0,0 +1,46 @@
+export enum Section {
+  Import = 'import',
+  Export = 'export',
+}
+
+export enum El {
+  Submit = 'submit',
+  Clear = 'clear',
+  Message = 'message',
+  Input = 'input',
+}
+
+export function element(
+  section: Section,
+  element: El.Submit,
+): HTMLButtonElement;
+export function element(section: Section, element: El.Clear): HTMLButtonElement;
+export function element(section: Section, element: El.Message): HTMLDivElement;
+export function element(
+  section: Section,
+  element: El.Input,
+): HTMLTextAreaElement;
+export function element(section: Section, element: El) {
+  const selector = `#${section} .${element}`;
+  const el = document.querySelector(selector);
+  switch (element) {
+    case El.Submit:
+    case El.Clear:
+      return el as HTMLButtonElement;
+    case El.Message:
+      return el as HTMLDivElement;
+    case El.Input:
+      return el as HTMLTextAreaElement;
+  }
+}
+
+export const resizeTextArea = (textArea: HTMLTextAreaElement) => {
+  textArea.style.height = 'auto';
+  textArea.style.height = `${Math.min(
+    textArea.scrollHeight,
+    window.innerHeight - 65,
+  )}px`;
+};
+
+export const setMessage = (section: Section, message: string) =>
+  (element(section, El.Message).innerText = message);
